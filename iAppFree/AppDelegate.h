edfa355765b146967331d50f88eb33bf6c5f591c
KyonLi <i@kyonli.com>
2015-05-22 17:26:27 +0800

    Initial Commit

---
+++ iAppFree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAppFree
+//
+//  Created by Kyon on 15/5/22.
+//  Copyright (c) 2015年 Kyon Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
